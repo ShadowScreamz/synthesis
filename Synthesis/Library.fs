31ac0b005f9a0e1022721e2b17fb3841fd56e33e
Tshegofatso Molobe <G17M9848@ict.ru.ac.za>
2019-03-12 15:47:59 +0200

    changes made

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -50,43 +50,101 @@ let ofTime h m s =
     failwith "Not implemented"
 
 let toTime sec =
-     let h = sec % 3600
-     let hh = h * 60
-     let m = hh % 60
-     let mm = m * 60
-     let s = sec - hh - mm
-     //sec = (h, m ,s )
-     failwith "Not implemented"
+    let h = sec / 3600
+    let m = (sec / 60) % 60
+    let s = (sec - (h * 3600)) % 60
+    match sec <= 0 with 
+    |true -> (0,0,0)
+    |_ -> (h, m, s)
+//failwith "Not implemented"
 
 let digits input =
-    let rec count num =
-        match num >= - 9 || num <= 9 with
-        |true -> 1
-        |_ -> 
-            match num / 10 >= 0 with
-            |true -> 1 + count(num + 1)
-            |_ -> 
-            failwith "Not implemented"
-    count input
+    let rec count num acc=
+        match num / 10 = 0 with
+            |true -> acc
+            |_ -> count (num / 10) (acc + 1)
+    count input 1
+//failwith "Not implemented"
+
    
 
-let minmax _ =
-    failwith "Not implemented"
+let minmax (a,b,c,d) =
+    let x = min(min a b) (min c d) 
+    let y = max(max a b) (max c d)
+    x, y
+failwith "Not implemented"
 
-let isLeap _ =
-    failwith "Not implemented"
+let isLeap leap =   match leap < 1582 with
+                    |true -> failwith "Not implemented"
+                    |false ->   match (leap % 4 = 0 && not(leap % 100 = 0)) ||  (leap % 4 = 0 && leap % 400 = 0)  with
+                                |true -> true
+                                |false -> false
 
-let month _ =
-    failwith "Not implemented"
+let month monthday =    match monthday < 1 || monthday > 12 with
+                        |true -> failwith "Not implemented"
+                        |false ->   match monthday with  
+                                    |1 -> "January", 31 
+                                    |2 -> "February", 28
+                                    |3 -> "March", 31 
+                                    |4 -> "April", 30 
+                                    |5 -> "May", 31 
+                                    |6 -> "June", 30
+                                    |7 -> "July", 31 
+                                    |8 -> "August", 31
+                                    |9 -> "September", 30 
+                                    |10 -> "October", 31 
+                                    |11 -> "November", 30
+                                    |12 -> "December", 31 
+        
 
-let toBinary _ =
-    failwith "Not implemented"
 
-let bizFuzz _ =
-    failwith "Not implemented"
+let toBinary dec =
+     match dec < 0 with
+     |true -> failwith "Not Implemented"
+     |false ->  let rec dectobin deci acc =
+                 match deci = 0 with 
+                 |true ->  
+                    match acc with 
+                    |"" -> "0"
+                    |_ -> acc
+                 |false ->
+                    let rem = deci % 2
+                    match rem = 0 with
+                    |true -> dectobin(deci / 2) ("0" + acc)
+                    |false -> dectobin(deci / 2) ("1" + acc)
+                dectobin dec ""
 
-let monthDay _ _ =
-    failwith "Not implemented"
+
+    
+    //failwith "Not implemented"
+let bizFuzz x =
+    let rec count a b c d =
+        match a>x || x<0 with
+        |true -> (b,c,d)
+        |false ->   match (a%3=0 && a%5=0) with
+                    |true -> count (1+a) (1+b) (1+c) (1+d)
+                    |false ->   match a%3=0 with
+                                |true -> count (1+a) (1+b) (c) (d)
+                                |false ->   match a%5=0 with
+                                            |true -> count (1+a) (b) (1+c) (d)
+                                            |false -> count (1+a) b c d
+    match x<0 with
+    |true -> (0,0,0)
+    |false -> count 1 0 0 0
+
+
+
+let monthDay d y =
+   (* let days = d / 30
+    match d < 1 || d > 367 || y < 1582 with
+    |true -> failwith "Not Implemented"
+    |false ->   match (d>0 || d < 366 && days > 0, isLeap(y)= false) with
+                |true, true  -> month(days)
+                |false, true -> month((days+1)
+                |false, false ->    match (d > 0 || d < 367 && days > 0, isLeap(y) = true) with
+                                    |true, true -> month(days)
+                                    |false, true -> month(days + 1) *)
+ failwith "Not implemented"  
 
 let coord _ =
     failwith "Not implemented"
